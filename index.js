9d755a6339f5b92b5114290e754382a1768f2674
vakho-naro <vakhonaro123@gmail.com>
2019-06-20 15:18:48 +0400

    changes

--- index.js
+++ index.js
@@ -46,15 +46,15 @@ function currentDiv(n) {
   function showDivs(n) {
     var i;
     var x = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("demo");
+    var dots = document.getElementsByClassName("gallery__section--photos");
     if (n > x.length) {slideIndex = 1}
     if (n < 1) {slideIndex = x.length}
     for (i = 0; i < x.length; i++) {
       x[i].style.display = "none";
     }
     for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" w3-opacity-off", "");
+      dots[i].className = dots[i].className.replace(" gallery__section--opacity-off", "");
     }
     x[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " w3-opacity-off";
+    dots[slideIndex-1].className += " gallery__section--opacity-off";
   }
